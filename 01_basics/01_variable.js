c94d7cf17d98e4c4e662b11d2fd086a5f6b644c1
maheshratta <130710541+VISMAH@users.noreply.github.com>
2025-02-02 05:43:32 +0000

    variable done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId= 66656
+let accountemial="maheshbhagat@6gmail.com"
+
+var accountpasswrd= " 12345"
+
+accountCity= "jammu"
+
+let accountstate;
+
+accountemial= "abc@gmail.com"
+accountpasswrd= "12345"
+accountCity= "chandigarh "
+
+console.log(accountId);
+console.table([accountId,accountemial,accountpasswrd,accountCity,accountstate])
