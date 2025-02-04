cf24949248fa3de6435c86bed943fbbfd9b181ca
maheshratta <130710541+VISMAH@users.noreply.github.com>
2025-02-04 17:47:55 +0000

    nums and maths

--- 01_basics/sring.js
+++ 01_basics/sring.js
@@ -2,8 +2,24 @@ const name= "mahesh"
 
 const repocount= 50
 
+const gamename= new String('maheshratta')
+
 console.log(name+repocount+"value");
 
 console.log(`my name is ${name} and my repo count is ${repocount}`);
 
 
+ console.log(gamename.charAt(5));
+
+ const newstring = gamename.substring(0,6)
+
+ console.log(newstring);
+
+ const newstringone= "            love you            "
+
+ console.log(newstringone);
+ console.log(newstringone.trim());
+
+ const url= "https://mahesh.com/mahesh%35ratta"
+
+ console.log(url.replace('%35','-'));
