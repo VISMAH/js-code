3e32d841affff2282c40a5f55cd75e0df04b29a7
maheshratta <130710541+VISMAH@users.noreply.github.com>
2025-02-05 16:54:34 +0000

    array

---
+++ 02_basics/array2.js
@@ -0,0 +1,23 @@
+const marvel=['thor','ironman','loki']
+
+const dc= ['batman','superman','wwpmen']
+
+
+// marvel.push(dc)
+// console.log(marvel);
+
+// const allmarvel=marvel.concat(dc);
+// console.log(allmarvel);
+
+
+//  spred operator 
+// const allmarvel= [...marvel,...dc]
+// console.log(allmarvel)
+
+// const arr= [1,2,[3,4,4],6,7,[8,9,10]]
+
+// console.log(arr.flat(Infinity))
+
+console.log(Array.isArray('mahesh'))
+console.log(Array.from('mahesh'))
+console.log(Array.from({name:'mahesh'}))
