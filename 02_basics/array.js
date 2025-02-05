434c756793cb96e2681e0272fade29d759aaf1e4
maheshratta <130710541+VISMAH@users.noreply.github.com>
2025-02-05 16:36:56 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,19 @@
+// array
+
+
+const myarr= [1,2,3,5,'mahesh']
+
+console.log(myarr)
+
+console.log(myarr[4])
+
+
+myarr.push(9)
+
+console.log(myarr)
+
+console.log(myarr.indexOf('mahesh'))
+
+const newarr=myarr.join()
+console.log(myarr)
+console.log(newarr)
