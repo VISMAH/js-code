9b7b7b3d6293920427c56cc1cfc64c88c0ec387e
maheshratta <130710541+VISMAH@users.noreply.github.com>
2025-02-08 17:21:56 +0000

    function

---
+++ 03_basics/function.js
@@ -0,0 +1,22 @@
+function cal(...num1){
+
+    return num1;
+}
+
+console.log(cal(6,3,3,3,3))
+
+
+const user ={
+
+    udername:"mahesh",
+    age:'34'
+
+
+}
+
+function handleobject(anyobject){
+
+    console.log(`username is ${anyobject.udername} and age is${anyobject.age}`);
+
+}
+handleobject(user);
