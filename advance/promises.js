36b00ffaffcb0d0e9c177ef98186ec5bbc12c3aa
VISMAH <maheshratta@gmail.com>
2025-03-08 23:51:18 +0530

    promise

---
+++ advance/promises.js
@@ -0,0 +1,39 @@
+const promise1 = new Promise(function(resolve, reject){
+     
+    
+    setTimeout(function(){
+        
+        console.log('assyng the task ');
+
+        resolve()
+    },1000)
+
+
+});
+
+
+promise1.then(function(){
+    console.log("task is done ")
+})
+
+
+new promise1(function(resolve,reject) {
+    setTimeout(function(){
+        console.log('assyng  2 the task ')
+        resolve();
+    },1000)
+
+}).then(function(){
+    console.log("task 2 is done ")
+})
+
+
+/// 
+async function getAllUser(params) {
+     const response= await fetch('https://api.github.com/users');
+      const data =response.json();
+      console.log(data);
+
+    
+}
+getAllUser();
