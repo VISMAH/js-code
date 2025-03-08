dfa41ec0dece371779d6c19f35482d25e23223b7
VISMAH <maheshratta@gmail.com>
2025-03-08 23:56:21 +0530

    promise2

--- advance/promises.js
+++ advance/promises.js
@@ -1,39 +1,49 @@
-const promise1 = new Promise(function(resolve, reject){
+// const promise1 = new Promise(function(resolve, reject){
      
     
-    setTimeout(function(){
+//     setTimeout(function(){
         
-        console.log('assyng the task ');
+//         console.log('assyng the task ');
 
-        resolve()
-    },1000)
-
-
-});
+//         resolve()
+//     },1000)
 
 
-promise1.then(function(){
-    console.log("task is done ")
-})
+// });
 
 
-new promise1(function(resolve,reject) {
-    setTimeout(function(){
-        console.log('assyng  2 the task ')
-        resolve();
-    },1000)
-
-}).then(function(){
-    console.log("task 2 is done ")
-})
+// promise1.then(function(){
+//     console.log("task is done ")
+// })
 
 
-/// 
-async function getAllUser(params) {
-     const response= await fetch('https://api.github.com/users');
-      const data =response.json();
-      console.log(data);
+// new promise1(function(resolve,reject) {
+//     setTimeout(function(){
+//         console.log('assyng  2 the task ')
+//         resolve();
+//     },1000)
+
+// }).then(function(){
+//     console.log("task 2 is done ")
+// })
+
+
+// /// 
+// async function getAllUser(params) {
+//      const response= await fetch('https://api.github.com/users');
+//       const data =response.json();
+//       console.log(data);
 
     
-}
-getAllUser();
+// }
+// getAllUser();
+
+
+fetch('https://api.github.com/users')
+.then((Response)=>{
+        return Response.json();  
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((Error) =>console.log(Error))
